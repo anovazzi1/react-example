b69b642b0c0c7c444a6bbaea2c49d1330c819c11
otavioanovazzi <otavio2204@gmail.com>
2022-08-17 10:22:20 -0300

    responsive space in predictions

--- src/components/TableRowComponents/Predictions.js
+++ src/components/TableRowComponents/Predictions.js
@@ -3,20 +3,24 @@ import Badge from "../../Badge"
 export default function Predictions()
 {
     return (
-        <td className="">
-            <div className="flex h-full">
-                <div className="flex flex-col">
-                    <Badge color={"red"} name="soccer"></Badge>
-                    <Badge color={"red"} name="soccer"></Badge>
-                    <Badge color={"red"} name="soccer"></Badge>
-                </div>
-                <div className="flex flex-col">
-                    <Badge color={"red"} name="99%"></Badge>
-                    <Badge color={"red"} name="99%"></Badge>
-                    <Badge color={"red"} name="99%"></Badge>
-                </div>
+        <td className="table h-32 border-spacing-1">
+                <tr className="border-hidden">
+                    <td><Badge color={"red"} name="soccer"></Badge></td>
+                    <td><Badge color={"red"} name="98%"></Badge></td>
+                </tr>
+                <tr>
+                    <td><Badge color={"blue"} name="soccer"></Badge></td>
+                    <td><Badge color={"blue"} name="98%"></Badge></td>
+                </tr>
+                <tr>
+                    <td><Badge color={"orange"} name="soccer"></Badge></td>
+                    <td><Badge color={"orange"} name="98%"></Badge></td>
+                </tr>
+                <tr>
+                    <td><Badge color={"green"} name="soccer"></Badge></td>
+                    <td><Badge color={"green"} name="98%"></Badge></td>
+                </tr>
 
-            </div>
         </td>
     )
 }
